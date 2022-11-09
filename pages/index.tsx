d2ae65d74b9da2cbe4e4457498e9a74a64d792bb
Hemesh Patel <hemesh.v.patel@gmail.com>
2022-11-09 17:13:17 -0600

    cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hemesh Patel's Portfolio</title>
+        <meta name="description" content="Created by Hemesh using Next.js" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Portfolio Build</h1>
+    </div>
+  )
+}
