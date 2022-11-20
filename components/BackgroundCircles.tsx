34c8f08c6ba59e24d2ab91ea8c32404b0425524f
Hemesh Patel <hemesh.v.patel@gmail.com>
2022-11-19 23:40:00 -0600

    circles color change

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -14,11 +14,11 @@ export default function BackgroundCircles({}: Props) {
       transition={{ duration: 2.5 }}
       className="relative flex justify-center items-center"
     >
-      <div className="absolute border border-#333333 rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="absolute border border-#333333 rounded-full h-[300px] w-[300px] mt-52" />
-      <div className="absolute border border-#333333 rounded-full h-[500px] w-[500px] mt-52" />
+      <div className="absolute border border-gray-300 rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-gray-300  rounded-full h-[300px] w-[300px] mt-52" />
+      <div className="absolute border border-gray-300  rounded-full h-[500px] w-[500px] mt-52" />
       <div className="absolute border border-red-500 opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse" />
-      <div className="absolute border border-#333333 rounded-full h-[800px] w-[800px] mt-52" />
+      <div className="absolute border border-gray-300  rounded-full h-[800px] w-[800px] mt-52" />
     </motion.div>
   );
 }
